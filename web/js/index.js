317a8f988385e409794a45726b5fced427b29e7c
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2023-12-03 19:40:12 +0200

    Major changes (see the description)
    Added function that dynamically load the marquee text; Changed the size of logo; Changed the layout of the navbar; Added the navbar toggler button; Changed the font weight of navbar; Changed the background image of body to webp format to speed the website up; Added noscript tag; Added text-accent-yellow and text-accent-blue classes, as well as bold-400; Major changes to index.css; Downloaded and moved to js/ folder all bootstrap and jquery min js files.

---
+++ web/js/index.js
@@ -0,0 +1,34 @@
+
+const AddTextToMarqueeTextBlock = () => {
+    const textBlocks = document.getElementsByClassName("marquee-animation-text-block")
+
+    // Required text parts to fill up the width of the page.
+    const textParts = 40
+
+    const PlaceText = (j, i) => {
+        const color = j % 2 == 0 ? "blue" : "yellow"
+
+        if (i % 2 == 0) {
+            textBlocks[j].innerHTML += `<span class="text-accent-${color} text-animation">Додаткова освіта</span>`
+        } else {
+            textBlocks[j].innerHTML += `<span class="text-animation">Додаткова освіта</span>`
+        }
+    }
+
+    for (let j = 0; j < textBlocks.length; j++) {
+        if (j % 2 == 0) {
+            for (let i = 0; i < textParts; i++) {
+                PlaceText(j, i)
+            }
+        } else if (j % 2 != 0) {
+            for (let i = 0; i < textParts; i++) {
+                PlaceText(j, i)
+            }
+        }
+    }
+}
+
+const ChangePanda = () => {
+    //TODO: Image of Panda changes randomly after every reload
+    return
+}
